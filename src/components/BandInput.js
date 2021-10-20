8dcff249deb9fca914ef35300cd1a48d03a4029f
JSONRete <jsonrete@gmail.com>
2021-10-20 10:32:44 -0400

    commit tree 2599bd45686081b6159c58e895c53dae2235d596

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,11 +1,42 @@
 // Add BandInput component
 import React, { Component } from 'react'
+// import { dispatch } from 'react-redux'
 
 class BandInput extends Component {
+
+state = {
+    name: '',
+  };
+
+
+handleChange = (event) => {
+  this.setState({
+    name: event.target.value,
+  });
+};
+
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.addBand(this.state);
+    this.setState({
+      name: '',
+    });
+  }
+
   render() {
     return(
       <div>
-        Band Input
+        <form onSubmit={this.handleSubmit}>
+        <input
+        type="text"
+        placeholder="let the band play on"
+        onChange={this.handleChange}
+        value={this.state.name}
+       />
+        <input type="submit" />
+        </form>
+        {this.state.name}
       </div>
     )
   }
